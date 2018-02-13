bfecaeb89bce33dcb649b86e3e692cbcf461c272
Martin Silva <msmartinsilva@gmail.com>
2018-02-13 12:26:47 -0300

    Added Vanilla folder. Refactored main.js.

--- vanilla/main.js
+++ vanilla/main.js
@@ -1,34 +1,37 @@
-function listen(element, eventName){
-
+function listen(element, eventName, callback){
+  element.addEventListener(eventName, callback);
 }
 
 function isDone(element){
   return element.classList.contains("done");
 }
 
+
 function main(){
   const addButton = document.getElementById("add");
   const todoesDiv = document.getElementById("todoes");
+  const doneFilter = document.getElementById("done-filter");
+  const undoneFilter = document.getElementById("undone-filter");
+  const allFilter = document.getElementById("all-filter");
   
-  addButton.addEventListener("click", function(){  
+  function addButtonOnClick(){  
     const todo = document.getElementById("todo-text").value;
-    
+    const todoDiv = document.createElement("div");
     const a = document.createElement("a");
     const x = document.createElement("a");
-    const todoDiv = document.createElement("div");
     
     x.textContent = "x";
     a.textContent = todo + " ";
-
+    a.classList.add("undone");
+  
     todoDiv.appendChild(a);
     todoDiv.appendChild(x);
     todoesDiv.appendChild(todoDiv);
-
+  
     document.getElementById("todo-text").value = "";
-  });
-
-  todoesDiv.addEventListener("click", function(event){
-
+  } 
+  
+  function todoesDivOnClick(event){
     if(event.target.innerHTML === "x"){
       return event.target.parentNode.remove();  
     }
@@ -39,13 +42,18 @@ function main(){
         a.classList.remove("done");
       } else {
         a.classList.add("done");
+        a.classList.remove("undone");
       }
     }
+  } 
 
-
-  });
-
+  function doneFilterOnClick(){
+    
+  }
   
+  listen(addButton, "click", addButtonOnClick);
+  listen(todoesDiv, "click", todoesDivOnClick);
+  listen(doneFilter, "click", doneFilterOnClick);  
 }
 
 
